007126cc1835c4db9dc3a9541df49daffbe734d5
nahidMBSTU <nahid.mbstu.ict@gmail.com>
2017-11-06 11:05:46 +0600

    6/11/17

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
 <title>Visitor Management System</title>
 
 <!-- Styles -->
-<link href="{{asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link href="{{asset('css/custom.css')}}" rel="stylesheet">
 
 
